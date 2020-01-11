ac264ce676cfe065b28811330a0f0c07b92ae306
Levi Elliott <leviemr@knights.ucf.edu>
2020-01-11 18:49:49 -0500

    Commit 01

---
+++ Smith01/src/Example01.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Example01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Scanner input = new Scanner(System.in);
+			
+			System.out.println("Please enter your name: ");
+			String name = input.next();
+			
+			System.out.println("Hello, " + name);
+	}
+
+}
